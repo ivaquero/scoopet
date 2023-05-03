9f5686f3b4a885978d98754ac1cad48878d2469c
ivaquero <msaintms@outlook.com>
2023-05-03 12:03:59 +0800

    update ci

--- bin/formatjson.ps1
+++ bin/formatjson.ps1
@@ -1,4 +1,25 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
-$path = "$PSScriptRoot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$formatjson' -dir '$path' $($args | ForEach-Object { "$_ " })"
+<#
+.SYNOPSIS
+    Format manifests using scoop's formatter.
+.PARAMETER App
+    Manifest name.
+.PARAMETER Dir
+    Where to search for manifests.
+    Default to bucket folder.
+#>
+param(
+    [Parameter(ValueFromPipeline = $true)]
+    [Alias('App')]
+    [String[]] $Manifest = '*',
+    [ValidateScript( { if ( Test-Path $_ -Type Container) { $true } else { $false } })]
+    [String] $Dir = "$PSScriptRoot\..\bucket"
+)
+
+begin {
+    if (-not $env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+    $Dir = Resolve-Path $Dir
+}
+
+process { foreach ($man in $Manifest) { Invoke-Expression -Command "$env:SCOOP_HOME\bin\formatjson.ps1 -App ""$man"" -Dir ""$Dir""" } }
+
+end { Write-Host 'DONE' -ForegroundColor Yellow }
