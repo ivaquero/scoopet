b772b878e2825274b883a2649d9f9e2d496f6ac0
ivaquero <msaintms@outlook.com>
2025-03-03 12:56:19 +0800

    update README

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 5.1
 param(
     # overwrite upstream param
-    [String]$upstream = 'ivaquero/scoopet:master'
+    [String]$upstream = 'scoopforge/extras-cn:master'
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
